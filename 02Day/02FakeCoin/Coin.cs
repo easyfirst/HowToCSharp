a71fc4bdfc85670cfaa2ae611ff05e294cd3f793
rusky65 <->
2018-08-08 21:26:25 +0200

    #4: New project for presentation of derivation and functions (virtual/override), 02FakeCoin.

---
+++ 02Day/02FakeCoin/Coin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02FakeCoin
+{
+    public class Coin
+    {
+        Random random = new Random();
+
+        public int Collect()
+        {
+            return random.Next(0, 2);
+        }
+    }
+}
---
+++ 02Day/02FakeCoin/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02FakeCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coin = new Coin();
+
+            Console.WriteLine("Result of coin collection:");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write($"{coin.Collect()}, ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
