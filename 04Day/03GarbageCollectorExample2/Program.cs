92e74b38ebd0968489db1dac0a05f0836d357a75
rusky65 <->
2018-08-11 19:55:58 +0200

    #7: Example code for memory management and correct use of the finalizer (IDisposable). using(reference) { }

---
+++ 04Day/03GarbageCollectorExample2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+
+namespace _03GarbageCollectorExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (!Console.KeyAvailable)
+            {
+                //This code burdens the memory.
+                //var bitmap = new Bitmap(2048, 2048);
+
+                //If we pay attention to cleaning with help of finalizer we have no memory and processor problem.
+                using (var bitmap = new Bitmap(1280, 1024)) { }
+
+            }
+        }
+    }
+}
