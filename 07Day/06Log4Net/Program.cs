15ecc1eba88db510e98aeb82e113d0a8275b6a34
rusky65 <->
2018-08-29 22:29:43 +0200

    #12: Test program for simulate logging with Log4Net.

---
+++ 07Day/06Log4Net/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _06Log4Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            while (!Console.KeyAvailable) //It is running till a key is press down.
+            {
+                //We select a level randomly for logging.
+                var level = r.Next(100);
+                //We simulate working of a program.
+                //We would like more logs of low level. The worse the entry is, the less it will be
+                if (level < 50)
+                { //The lowest level, but most frequent messages (Debug)
+                }
+                if (level >= 50 && level < 70)
+                { //Info
+                }
+                if (level >= 70 && level < 85)
+                { //Warning
+                }
+                if (level >= 85 && level < 95)
+                { //Error
+                }
+                if (level >= 95)
+                { //Fatal
+                }
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
