625c6ed2644ae0abac39eb00a7ad321124f0be6e
rusky65 <->
2018-08-30 22:50:43 +0200

    #15: Project for demonstrate strategy pattern.

---
+++ 08Day/08StrategyPattern/DataStore.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _08StrategyPattern
+{
+    public class DataStore
+    {
+        private int[] data;
+
+        public DataStore(int[] data)
+        {
+            this.data = data;
+        }
+
+        public int SumOfOdd()
+        {
+            var sum = 0;
+            foreach (var d in data)
+            {
+                // We look for odd numbers.
+                if (d % 2 == 1)
+                {
+                    sum += d;
+                }
+            }
+
+            // with linq looks like:
+            //return data.Sum( x => x % 2 == 1 );
+
+            return sum;
+        }
+
+        public int ProductOfEven()
+        {
+            var prod = 1;
+
+            foreach (var d in data)
+            {
+                if (d % 2 == 0)
+                {
+                    prod *= d;
+                }
+            }
+
+            return prod;
+        }
+    }
+}
---
+++ 08Day/08StrategyPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _08StrategyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var store = new DataStore(data: new int[] {1, 3, 4, 5, 7, 8, 10, 15, 30});
+
+            var sum = store.SumOfOdd();
+
+            Console.WriteLine($"Sum  odd numbers: {sum}");  // 31
+
+            sum = store.ProductOfEven();
+
+            Console.WriteLine($"Multiplication eve numbers: {sum}");    // 9600
+
+            Console.ReadLine();
+        }
+    }
+}
