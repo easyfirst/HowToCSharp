9421dab094f1dee69f6011edda8d259bf5f92c45
rusky65 <->
2018-08-30 22:03:35 +0200

    #14: Downloading NuGet Serilog packages. Setting Logger static property.

--- 08Day/07Serilog/Program.cs
+++ 08Day/07Serilog/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using Serilog;
 
 namespace _07Serilog
 {
@@ -6,6 +8,13 @@ namespace _07Serilog
     {
         static void Main(string[] args)
         {
+            // The Serilog Logger static property is reachable from every class.
+            // The file location is: 07Serilog \ bin \ Debug \ netcoreapp2.1 \ serilog.log
+            Serilog.Log.Logger = new Serilog.LoggerConfiguration()
+                                            .WriteTo.Console()
+                                            .WriteTo.File(".\\serilog.log")
+                                            .CreateLogger();
+
             var r = new Random();
             while (!Console.KeyAvailable) //It is running till a key is press down.
             {
@@ -15,18 +24,23 @@ namespace _07Serilog
                 //We would like more logs of low level. The worse the entry is, the less it will be
                 if (level < 50)
                 { //The lowest level, but most frequent messages (Debug)
+                    Log.Logger.Debug($"Debug: {level}");
                 }
                 if (level >= 50 && level < 70)
                 { //Info
+                    Log.Logger.Information($"Information: {level}");
                 }
                 if (level >= 70 && level < 85)
                 { //Warning
+                    Log.Logger.Warning($"Warning: {level}");
                 }
                 if (level >= 85 && level < 95)
                 { //Error
+                    Log.Logger.Error($"Error: {level}");
                 }
                 if (level >= 95)
                 { //Fatal
+                    Log.Logger.Fatal($"Fatal: {level}");
                 }
                 Thread.Sleep(200);
             }
