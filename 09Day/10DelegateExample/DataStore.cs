737524376f8dc36da808f8d951ae0daea6329f05
rusky65 <->
2018-09-05 22:47:25 +0200

    #16: Using delegate call list.  - with multiple function calls in the call list  - with data modifier functions  - with environmental variables  - with anonymous delegate

---
+++ 09Day/10DelegateExample/DataStore.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10DelegateExample
+{
+    public class DataStore
+    {
+        private List<string> lines;
+
+        /// <summary>
+        /// Function definition to enable the modifying function (s) (strategies) to come in with the help of delegate
+        /// </summary>
+        /// <param name="text"></param>
+        public delegate void FuncDef(ref string text);
+
+        public DataStore(List<string> lines)
+        {
+            this.lines = lines;
+        }
+
+        public void Print()
+        {
+            foreach (var item in lines)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        /// <summary>
+        /// The interface of the modifying
+        /// during this "face" arrive the "list" of the changes
+        /// </summary>
+        /// <param name="processList">
+        /// The delegate's call list, with each element pointing to such a function,
+        /// which corresponds to the FuncDef definition.
+        /// </param>
+        public void ProcessData(FuncDef processList)
+        {
+            ///We have to use the 'for' cycle instead of the 'foreach' cycle,
+            ///because inside the cycle of 'foreach' the item is not enable to modify.
+            for (int i = 0; i < lines.Count; i++)
+            {
+                // We get the value fo the lines[i].
+                var item = lines[i];
+
+                // We send the item into the function.
+                processList(ref item);
+
+                // We write back the modified value of the item into the list.
+                lines[i] = item;
+            }
+        }
+    }
+}
---
+++ 09Day/10DelegateExample/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10DelegateExample
+{
+    // Protecting delegate call list against a bad start value
+
+    class Program
+    {
+        static string CharToRemove = "m";
+
+        static void Main(string[] args)
+        {
+            var lines = new List<string>();
+            lines.Add("First item.");
+            lines.Add("Second item.");
+            lines.Add("Thirth item.");
+            lines.Add("Forth item.");
+            lines.Add("Fifth item.");
+            lines.Add("Sixth item.");
+
+            var store = new DataStore(lines);
+
+
+            store.ProcessData(RemoveT);
+            store.ProcessData(RemoveF);
+
+            /// The call list is not "just "definition of function but "environment "as well.
+            /// I can refer to a local variable and it is going with the call list.
+            store.ProcessData(RemoveChar);
+
+
+            /// The call list is called up one after the other
+            /// and with the same parameter variables.
+            /// So the variable passed in the ref is also the same, that's why I can do more than one operation.
+
+
+            /// I can handle previous operations as well,
+            /// so put the calls to a list and pass the list.
+            DataStore.FuncDef processList;
+
+            // for example:
+            //processList = RemoveT;
+
+            // for example:
+            //processList = processList + RemoveF + RemoveChar;
+
+            // or :
+
+            processList = delegate { };     // I define a blank call list, it works without any type.
+
+            processList += RemoveT;
+            processList += RemoveF;
+
+            // the call list is taking the reference with.
+            processList += RemoveChar;
+            //store.ProcessData(processList);
+
+
+
+            // if it changes, it will affect its functioning.
+            CharToRemove = "h";
+
+
+            // if you want to send a local variable to the function,
+            // then you can use it instead of a separate function definition, using an anonymous delegate:
+            processList += delegate (ref string toModify)
+            {
+                var toReplace = "d";
+                toModify = toModify.Replace(toReplace, "");
+            };
+
+            // The function defined within the code block has access to the variables within the code block (for example: toReplace).
+            var toReplaceVar = "o";
+            processList += delegate (ref string toModify)
+            {
+                toModify = toModify.Replace(toReplaceVar, "");
+            };
+            store.ProcessData(processList);
+
+
+            store.Print();
+
+            Console.ReadLine();
+        }
+
+        private static void RemoveF(ref string text)
+        {
+            text = text.Replace("f", "");
+        }
+
+        private static void RemoveT(ref string text)
+        {
+            text = text.Replace("t", "");
+        }
+
+        private static void RemoveChar(ref string text)
+        {
+            /// I can also use a parameter from the outside at the delegate: what the definition can look ,
+            /// and it is going with the call list.
+            text = text.Replace(CharToRemove, "");
+        }
+
+    }
+}
