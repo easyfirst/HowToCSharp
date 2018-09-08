36b1e3f8df9c200a9b7ae1b5e05c6b7c5a232fea
rusky65 <->
2018-09-08 13:25:21 +0200

    #16: Action and Func.

--- 09Day/11FuncActionLambda/Program.cs
+++ 09Day/11FuncActionLambda/Program.cs
@@ -47,6 +47,47 @@ namespace _11FuncActionLambda
 
             Console.WriteLine($"square of 2: {squaringProcesslist(2)}");
 
+            /////////////////////////////////////
+            //2. reduction step: Action and Func
+            /////////////////////////////////////
+
+            //"Action" and "Func" are predefined "delegates" that help eliminate point 1.
+            //
+            //the Action <> defines a void type delegate
+            //since no return value is required (void)
+            //no name is required (I write the "Action" where I would write the name, it is just a reference)
+            //the names of the parameters are only formal, 
+            //here is what I need to list the type of parameters
+
+            // the
+            // Action<T1,T2,...,TN> 
+            // equivalent of a  
+            // delegate void XXX(T1 t1, T2 t2, ... , T3 t3) 
+            // with the defibition, what developed by the developers of the .NET environment to the object directory.
+
+            Action<int, int> multiplicationProcessList = (a, b) => Console.WriteLine($"The result of multiplication: {a * b}");
+
+            //or with code block:
+            multiplicationProcessList = (a, b) =>
+            {
+                Console.WriteLine($"The result of multiplication: {a * b}");
+            };
+
+            //from the zero input parameter to the incoming parameter 16 this is part of the .NET environment:
+            //Action 
+            //Action<int, int, int, ... int>
+
+            // The "Func" is a definition of delegate returning with no void:
+            // It is the same as "Action", only the last type parameter is the return value of the function:
+
+            Func<int, int> squaringProcessList2 = x => x * x;
+
+            Console.WriteLine($"square of 3: {squaringProcessList2(3)}");
+
+            Func<int, int, string> multiplicationProcessList2 = (i, j) => $"{i} and {j} multiplication: {i * j}";
+
+            Console.WriteLine(multiplicationProcessList2(5, 6));
+
 
             Console.ReadLine();
         }
