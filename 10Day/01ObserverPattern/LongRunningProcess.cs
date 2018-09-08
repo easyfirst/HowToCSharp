6aae2c9d7abbbaf11da1cfcab3a679407a2651d1
rusky65 <->
2018-09-08 15:52:26 +0200

    #17: The "LongRunningProcess" is able to notify so many observers as it is givin in the params list.  - thanks for the keyword "params" using in LongRunningProcess constructor

--- 10Day/01ObserverPattern/LongRunningProcess.cs
+++ 10Day/01ObserverPattern/LongRunningProcess.cs
@@ -10,13 +10,11 @@ namespace _01ObserverPattern
     /// </summary>
     public class LongRunningProcess
     {
-        private IMessage log;
-        private IMessage ui;
+         private readonly IMessage[] observers;
 
-        public LongRunningProcess(IMessage log, IMessage ui)
+        public LongRunningProcess(params IMessage[] observers)
         {
-            this.log = log;
-            this.ui = ui;
+            this.observers = observers ?? throw new ArgumentNullException(nameof(observers)) ;
         }
 
         public void Start()
@@ -24,33 +22,40 @@ namespace _01ObserverPattern
             Console.WriteLine("LongRunningProcess: 0%");
 
             //todo: notify the curious participants (Observer)
-            log.Message(0);
-            ui.Message(0);
+            SendMessage(0);
             Console.WriteLine();
 
             Thread.Sleep(1000);
             Console.WriteLine("LongRunningProcess: 25%");
-            log.Message(25);
-            ui.Message(25);
+            SendMessage(0);
             Console.WriteLine();
 
             Thread.Sleep(1000);
             Console.WriteLine("LongRunningProcess: 50%");
-            log.Message(50);
-            ui.Message(50);
+            SendMessage(0);
             Console.WriteLine();
 
             Thread.Sleep(1000);
             Console.WriteLine("LongRunningProcess: 75%");
-            log.Message(75);
-            ui.Message(75);
+            SendMessage(0);
             Console.WriteLine();
 
             Thread.Sleep(1000);
             Console.WriteLine("LongRunningProcess: 100%");
-            log.Message(100);
-            ui.Message(100);
+            SendMessage(0);
             Console.WriteLine();
+        }
+
+        /// <summary>
+        /// It will notify all observers.
+        /// </summary>
+        /// <param name="data">The information what is needed to be sent</param>
+        private void SendMessage(int data)
+        {
+            foreach (var observer in observers)
+            {
+                observer.Message(data);
+            }
         }
     }
 }
--- 10Day/01ObserverPattern/Program.cs
+++ 10Day/01ObserverPattern/Program.cs
@@ -18,6 +18,13 @@ namespace _01ObserverPattern
             //the long-term process
             var process = new LongRunningProcess(log, ui);
 
+            // So you can use the function, thanks for the keyword "params":
+            //process = new LongRunningProcess();
+            //process = new LongRunningProcess(log);
+            //process = new LongRunningProcess(log, ui, log, log);
+
+
+
             process.Start();
 
             Console.WriteLine("The process is done.");
