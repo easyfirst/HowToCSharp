b242a4417bc94bd2ee8aed44228dc2d61078980f
rusky65 <->
2018-09-13 22:17:30 +0200

    #18: Handling Path on Windows and on Linux.

---
+++ 11Day/01LocalData/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace _01LocalData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Path is a static class that helps you in paths.
+
+            // you should normally abstain from the absolute paths, instead you should use a relative path.
+
+            // for example: relativ path
+            var path = "MyOneDirectory";
+
+            //and absolute path
+            Console.WriteLine($"This is a relativ path: {path}\n");
+            Console.WriteLine($" and this is the absolute path of the path above: {Path.GetFullPath(path)}");
+
+            Console.ReadLine();
+        }
+    }
+}
