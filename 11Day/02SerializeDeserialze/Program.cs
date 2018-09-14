26710e117cc5376f67d816736ac8d0b243f9fb3d
rusky65 <->
2018-09-14 07:16:18 +0200

    #18: Serialize / Deserialize basic task

---
+++ 11Day/02SerializeDeserialze/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02SerializeDeserialze
+{
+    class Program
+    {
+        public static DateTime DateTime { get; private set; }
+
+        static void Main(string[] args)
+        {
+            var data = new DataClass()
+            {
+                Text = "Ez itt egy árvíztűrő tükörfúrógép",
+                Integer = int.MaxValue,
+                DateTime = DateTime.MinValue,
+                Double = Math.PI
+
+            };
+
+            var dataList = new DataList();
+            dataList.DataClass = data;
+            dataList.Data.Add(data);
+            dataList.Data.Add(data);
+            dataList.Data.Add(data);
+            dataList.Data.Add(data);
+
+            Console.WriteLine("Hello World!");
+        }
+
+
+        public class DataClass
+        {
+            public int Integer { get; set; }
+            public double Double { get; set; }
+            public DateTime DateTime { get; set; }
+            public string Text { get; set; }
+        }
+
+
+        public class DataList
+        {
+            public DataList()
+            {
+                Data = new List<DataClass>();
+            }
+            public List<DataClass> Data { get; set; }
+            public DataClass DataClass { get; set; }
+        }
+    }
+}
