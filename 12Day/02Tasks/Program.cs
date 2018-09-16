02f9087099336311705317f97638b04ec97fbe26
rusky65 <->
2018-09-16 18:38:29 +0200

    #23: Statuses of Task, TaskStatus.

---
+++ 12Day/02Tasks/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _02Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action todo = () =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"i: {i}");
+                    Thread.Sleep(300);
+                }
+            };
+
+            //var task = new Task(todo2);
+
+            var task = new Task(todo);
+
+            Console.WriteLine($"Status: {task.Status}");
+            task.Start(); // Starting the task
+
+            Console.WriteLine($"Status: {task.Status}");
+            Thread.Sleep(100); // waiting a bit
+
+            Console.WriteLine($"Status: {task.Status}");
+            task.Wait(); // waiting till the end of task
+
+            Console.WriteLine($"Status: {task.Status}");
+
+            Console.ReadLine();
+
+            //Status: Created
+            //Status: WaitingToRun
+            //i: 0
+            //Status: Running
+            //i: 1
+            //i: 2
+            //i: 3
+            //i: 4
+            //i: 5
+            //i: 6
+            //i: 7
+            //i: 8
+            //i: 9
+            //Status: RanToCompletion
+        }
+
+        /// <summary>
+        /// Instead of action you can declare a function definition like this
+        /// </summary>
+        static public void todo2()
+        { }
+
+    }
+}
